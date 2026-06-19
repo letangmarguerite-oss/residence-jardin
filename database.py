440c2d25a425697746affcb40639af0bfb1ddd56
Margot <letang.marguerite@gmail.com>
2026-06-19 16:19:17 +0200

    database.py

--- database.py
+++ database.py
@@ -1,12 +1,16 @@
 import os
 import psycopg2
+import sqlite3
 from psycopg2.extras import RealDictCursor
 
 def get_connection():
-    return psycopg2.connect(
-        os.getenv("DATABASE_URL"),
-        cursor_factory=RealDictCursor
-    )
+    db_url = os.getenv("DATABASE_URL")
+
+    if db_url:
+        return psycopg2.connect(db_url, cursor_factory=RealDictCursor)
+    else:
+        return sqlite3.connect("local.db")
+
 
 def init_db():
     conn = get_connection()
